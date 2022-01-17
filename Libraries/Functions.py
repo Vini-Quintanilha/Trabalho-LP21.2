808c855fab081ca57fa27f2ef3a34065e55843cd
Tonny-Francis <tonnyfrancis161@poli.ufrj.br>
2022-01-17 14:26:28 -0300

    start

--- Libraries/Functions.py
+++ Libraries/Functions.py
@@ -1,1 +1,16 @@
-teste = 1
+import os
+
+#Recolhe os nomes da ações de um txt, afim de realizar busca.
+def Lista_Acoes():
+    lista = []
+
+    with open('Files/Actions.txt', 'r') as arquivo:
+        for linha in arquivo:
+            lista.append(linha)
+        arquivo.close()
+
+    return lista
+
+#Verifica se a ação desejada consta na lista
+def Busca_Acao(lista, nome):
+    pass
