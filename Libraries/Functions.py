dcc7806dec0e23d49450887e54127e20377d8ec3
Tonny-Francis <tonnyfrancis161@poli.ufrj.br>
2022-01-20 11:52:25 -0300

    Tabela

--- Libraries/Functions.py
+++ Libraries/Functions.py
@@ -1,6 +1,6 @@
 from numpy import source
 from pandas_datareader import data as web
-from datetime import datetime
+from datetime import datetime, timedelta
 
 #Recolhe os nomes da ações de um txt, afim de realizar busca.
 def Lista_Acoes():
@@ -25,9 +25,21 @@ def Busca_Acao(lista, nome):
     return 0
 
 #Obtem uma tabela com os preços do dia atual
-def Valor_Acao(acao):
+# --> True para abter apenas o valor da ação no presente momento
+# --> False para obter uma tabela de valores semanal para análise e gráficos
+def Tabela_acao(acao, funcao):
     acao = acao + '.SA'
 
     data_atual = datetime.today().strftime('%m-%d-%y')
-    valor_acao = web.DataReader(acao, data_source='yahoo', start=data_atual)
-    return  valor_acao
+    data_final = datetime.today() + timedelta(days=-6)
+    data_final = data_final.strftime('%m-%d-%y')
+
+    tabela = web.DataReader(acao, data_source='yahoo', start=data_final, end=data_atual)
+    
+    if funcao == True:
+        #print(len(tabela))
+        #valor = tabela['Close'].iloc[len(tabela)]
+        #print(valor)
+        pass
+
+Tabela_acao('PETR4', True)
