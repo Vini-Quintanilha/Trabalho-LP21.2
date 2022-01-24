b85e20f38cebcc42a7218636f719d2150ad5fddc
Tonny-Francis <tonnyfrancis161@poli.ufrj.br>
2022-01-24 15:50:12 -0300

    Correções

--- Libraries/Functions.py
+++ Libraries/Functions.py
@@ -1,6 +1,8 @@
 from numpy import source
 from pandas_datareader import data as web
 from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+import yfinance as yf
 
 #Recolhe os nomes da ações de um csv, afim de realizar busca.
 #OBS: Esse arquivo.csv pode ser encontrado no site da B3, 
@@ -19,7 +21,6 @@ def Lista_Acoes():
 
 #Verifica se a ação desejada consta na lista, caso conste será retornado
 #o nome da ação que consta na B3, caso contrário será retornado false
-"colocar um aviso ou uma opção de inserir novamente"
 def Busca_Acao(lista, nome):
     for linha in lista:
         if nome in linha:
@@ -28,23 +29,20 @@ def Busca_Acao(lista, nome):
             acao = linha[0:pos-1]
             return acao
     return 0
+    #Erro, caso o utilizador digite a ação errada será retornado um valor 0, onde será
+    #tratado na interface do tkinter
 
 #Obtem o valor do dia atual e uma tabela com os ultimos 5 dias (fora finais de semana).
 # --> True para obter apenas o valor da ação no presente momento
 # --> False para obter uma tabela de valores dos ultimos 5 dias para análise e gráficos
 def Tabela_acao(acao, funcao):
     acao = acao + '.SA'
-    dias = -4
 
-    data_atual = datetime.today().strftime('%m-%d-%y')
-    data_final = datetime.today() + timedelta(days=dias)
-    data_final = data_final.strftime('%m-%d-%y')
-
-    tabela = web.DataReader(acao, data_source='yahoo', start=data_final, end=data_atual)
-    
     if funcao == True:
-        valor = '{:.2f}'.format(tabela['Close'].iloc[len(tabela) - 1])
+        data_atual = datetime.today().strftime('%m-%d-%y')
+        valor = web.DataReader(acao, data_source='yahoo', start=data_atual)
+        valor = '{:.2f}'.format(valor['Adj Close'].iloc[0])
         return valor
     else:
+        tabela = yf.download(acao, period="5d")["Adj Close"]
         return tabela
-
