df78ff381393ce5eede97980f27c291a64345f6c
ViniQuintanilha <84692379+ViniQuintanilha@users.noreply.github.com>
2022-01-27 17:13:48 -0300

    testa

--- Libraries/Functions.py
+++ Libraries/Functions.py
@@ -16,26 +16,20 @@ def lista():
     with open('Files/IBOV.csv', 'r') as arquivo:
         for linha in arquivo:
             pos = linha.index(';')+1
-            acao = linha[0:pos-1]
+            acao = linha[0:pos-1] + '.SA'
             lista.append(acao)
             nome = linha[1:pos-1]
             tabela = pd.DataFrame(list(zip(nome,acao)), columns = ['Ticker','Nome'])
         arquivo.close()
 
+        tickers_empresas = tabela['Ticker']
+        lst_cotacao = []
+        for empresa in tickers_empresas:
+            cotacao = web.DataReader(empresa, data_source='yahoo', start=data_atual)
+            lst_cotacao.append(cotacao)
+            tabela["cotação"] = lst_cotacao
     return tabela
 
-#Verifica se a ação desejada consta na lista, caso conste será retornado
-#o nome da ação que consta na B3, caso contrário será retornado false
-def busca_Acao(lista, nome):
-    for linha in lista:
-        if nome in linha:
-            linha = str(linha)
-            pos = linha.index(';')+1
-            acao = linha[0:pos-1]
-            return acao
-    return 0
-    #Erro, caso o utilizador digite a ação errada será retornado um valor 0, onde será
-    #tratado na interface do tkinter
 
 #Obtem o valor do dia atual e uma tabela com os ultimos 5 dias (fora finais de semana).
 # --> True para obter apenas o valor da ação no presente momento
@@ -51,6 +45,21 @@ def tabela_acao(acao, funcao):
     else:
         tabela = yf.download(acao, period="5d")["Adj Close"]
         return tabela
+    tabela["cotação"] = 
+#Verifica se a ação desejada consta na lista, caso conste será retornado
+#o nome da ação que consta na B3, caso contrário será retornado false
+def busca_Acao(lista, nome):
+    for linha in lista:
+        if nome in linha:
+            linha = str(linha)
+            pos = linha.index(';')+1
+            acao = linha[0:pos-1]
+            return acao
+    return 0
+    #Erro, caso o utilizador digite a ação errada será retornado um valor 0, onde será
+    #tratado na interface do tkinter
+
+
 
 def ibovespa():
     ibov = yf.download('^BVSP', period="7y")["Adj Close"]
