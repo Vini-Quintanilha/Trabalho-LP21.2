11eb4332eef28da073e7824f375924c76f8b7f65
Tonny-Francis <tonnyfrancis161@poli.ufrj.br>
2022-02-07 22:18:31 -0300

    teste

---
+++ Testes.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+#Inicializa um Objeto Tk
+menu = Tk()
+
+#Alterar tamanho da janela e posição de onde vai aparecer
+menu.geometry("500x250+250+250")
+#Codigo aqui
+#---->
+menu.mainloop()
